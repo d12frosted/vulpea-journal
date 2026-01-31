7525fde77555d25cbe3b78fbe5cf35113b65e7cd
Boris Buliga <boris@d12frosted.io>
2026-01-31 15:42:10 +0200

    chore: prepare v1.0.1 release

--- vulpea-journal.el
+++ vulpea-journal.el
@@ -5,7 +5,7 @@
 ;;
 ;; Author: Boris Buliga <boris@d12frosted.io>
 ;; Maintainer: Boris Buliga <boris@d12frosted.io>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Package-Requires: ((emacs "29.1") (vulpea "2.0.0") (vulpea-ui "1.0.0") (dash "2.20.0"))
 ;; Keywords: org-mode, roam, convenience
 ;; URL: https://github.com/d12frosted/vulpea-journal
